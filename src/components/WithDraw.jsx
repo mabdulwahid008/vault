42f04e7300c89abf71c1c1fcff6770aaee64ac94
Muhammad Abdul Wahid <a.wahid@comtanix.local>
2025-02-05 11:44:26 -0800

    Commit

--- src/components/WithDraw.jsx
+++ src/components/WithDraw.jsx
@@ -18,48 +18,40 @@ const contract = getContract({
 function WithDraw() {
   const account = useActiveAccount();
 
-  
+
   const [amount, setAmount] = useState(0)
   const [loading, setLoading] = useState(false)
   const { signer } = useEthProvider();
- 
+
   const onClick = async () => {
     setLoading(true)
 
     try {
-      // const signer = await ethers5Adapter.signer.toEthers({ client, sepolia, account });
       const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
 
-      const tx = await contract.withdraw(2); 
+      const tx = await contract.withdraw(2);
       const receipt = await tx.wait();
-
-      // const transaction = prepareContractCall({
-      //   contract,
-      //   method: "function withdraw(uint256 amount)",
-      //   params: [amount], // type safe params
-      // });
-      // await sendTransaction(transaction);
     } catch (error) {
       console.log(error);
-      
+
     }
     setLoading(true)
   };
   return (
     <div className='flex justify-between items-center gap-3'>
-            <input
-                type='number'
-                placeholder='ENTER NUMBER OF TOKENS'
-                onChange={e => setAmount(Number(e.target.value))}
-                className='bg-themeblack2 px-4 py-2.5 placeholder:tracking-widest text-center outline-none font-bold text-white  text-sm w-full rounded-full'
-            />
-            <button
-            disabled={loading}
-            onClick={onClick} 
-            className='disabled:opacity-70 disabled:cursor-wait bg-themeGreen/80 hover:bg-themeGreen tracking-wider transition-all ease-in duration-300 cursor-pointer rounded-full px-4 py-2 text-white text-sm font-Pooppins font-semibold'>
-                Withdraw
-            </button>
-        </div>
+      <input
+        type='number'
+        placeholder='ENTER NUMBER OF TOKENS'
+        onChange={e => setAmount(Number(e.target.value))}
+        className='bg-themeblack2 px-4 py-2.5 placeholder:tracking-widest text-center outline-none font-bold text-white  text-sm w-full rounded-full'
+      />
+      <button
+        disabled={loading}
+        onClick={onClick}
+        className='disabled:opacity-70 disabled:cursor-wait bg-themeGreen/80 hover:bg-themeGreen tracking-wider transition-all ease-in duration-300 cursor-pointer rounded-full px-4 py-2 text-white text-sm font-Pooppins font-semibold'>
+        Withdraw
+      </button>
+    </div>
   )
 }
 
